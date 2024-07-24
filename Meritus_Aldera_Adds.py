12c8f4aed8c4594dac116e30464b4962126fd0db
Nkar541 <168180967+Nkar541@users.noreply.github.com>
2024-07-24 20:54:50 +0530

    Add files via upload

---
+++ Meritus_Aldera_Adds.py
@@ -0,0 +1,352 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 22:24:08 2022
+
+@author: GSantwani
+"""
+
+import pandas as pd
+import pyodbc
+import numpy as np
+filename = r"C:\Users\NKar\Music\Meritus_Adds_Nk.xlsx"
+
+
+df = pd.read_excel(filename,sheet_name = 'Sheet1')
+
+
+print('Kindly perform  data cleansing manually in order to match data values to data columns')
+print('Use the cleansed csv file in the path')
+
+# columns that dont change
+df.rename(columns={'Indiv. NPI#':'prov_npi_id', 'Last Name': 'prov_last_name', 'First Name': 'prov_first_name'}, inplace=True)
+df.rename(columns={'M I ':'prov_middle_initial', 'Degree': 'prov_prof_desg_1', 'Gender': 'prov_gender'}, inplace=True)
+df.rename(columns={'Ethnicity': 'prov_ethnicity','Languages':'prov_language_1'}, inplace=True)
+df.rename(columns={'Birth Date':'prov_birth_date'}, inplace=True)
+df.rename(columns={'Federal Tax ID':'tax_id_number','GROUP NPI #':'bu_npi_id','OFFICE NAME':'bu_name'}, inplace=True)
+df.rename(columns={'Mailing address (Billing)':'bu_address_1'}, inplace=True)
+df.rename(columns={'bill_city':'bu_city','bill_state':'bu_state','bill_zip':'bu_zip_code'}, inplace=True)
+
+df.rename(columns={'Address #1':'sl_address_1','Suite/Unit #':'sl_address_2','City ':'sl_city','State ':'sl_state'}, inplace=True)
+df.rename(columns={'Zip': 'sl_zip_code','Office County':'sl_county', 'Address #1':'sl_mail_address_1'}, inplace=True)
+df.rename(columns={'Suite/Unit #':'sl_mail_address_2','City':'sl_mail_city','State ':'sl_mail_state'}, inplace=True)
+df.rename(columns={'Zip':'sl_mail_zip_code','PRI Phone number':'sl_phone_number','PRI FAX':'sl_fax_number'}, inplace=True)
+df.rename(columns={'PCP/Specialist':'sl_status_code',})
+
+df['AgeLimits']=df['AgeLimits'].replace(np.nan,'0-100',regex=True)
+
+df[['Min_Age','Max_Age']] = df['AgeLimits'].str.split('-',expand=True)
+
+#add in blank columns
+df['prov_gender']=df['prov_gender'].replace(["Female","Male","female","male"],["F","M","F","M"])
+df['prov_name_suffix']=''
+df['prov_soc_sec_number']=df['SOC SEC #']
+df['prov_language_2']=''
+df['prov_email_address']=''
+df['prov_medicare_id']=''
+df['prov_medicaid_id']=df['Medicaid #']
+df['prov_cultural_competency_training']=''
+df['prov_dual_demo_population_training']=''
+df['prov_received_date']=''
+df['prov_approved_date']=''
+df['bu_name']=''
+df['bu_mail_address_1']=''
+df['bu_mail_city']=''
+df['bu_mail_state']=''
+df['bu_mail_zip_code']=''
+df['bu_payment_center']=''
+df['bu_business_type']='02'
+df['bu_address_2']=''
+df['bu_mail_address_2']=''
+df['sl_county']=''
+df['sl_mail_address_1']=''
+df['sl_mail_address_2']=''
+df['sl_mail_city']=''
+df['sl_mail_state']=''
+df['sl_mail_zip_code']=''
+df['sl_office_manager_name']=''
+df['sl_fax_number']=''
+df['sl_monday_hours']='8:00 AM-5:00 PM'
+df['sl_tuesday_hours']='8:00 AM-5:00 PM'
+df['sl_wednesday_hours']='8:00 AM-5:00 PM'
+df['sl_thursday_hours']='8:00 AM-5:00 PM'
+df['sl_friday_hours']='8:00 AM-5:00 PM'
+df['sl_saturday_hours']='CLOSED'
+df['sl_sunday_hours']='CLOSED'
+df['sl_status_code']=df['PCP/Specialist'].replace(["Specialist","PCP"],["N","Y"])
+df['sl_accept_new_patient']=df['Accepting New Patients'].replace(["No","Yes"],["N","Y"])
+df['sl_min_patient_age']=df['Min_Age']
+df['sl_max_patient_age']=df['Max_Age']
+df['sl_offered_services']=''
+df['sl_show_in_dir']=df['List in Directory'].replace(["No","Yes"],["N","Y"])
+df['sl_tty_service']=''
+df['sl_handicap_accessible']='Y'
+df['sl_accept_dev_disabilities']='Y'
+df['sl_panel_capacity']=''
+df['sl_termination_reason']=''
+df['sl_24_operation']=''
+df['sl_loc_category']=''
+df['sl_translation_services']=''
+df['sl_wheelchair_accessible_exam_room']=''
+df['sl_wheelchair_accessible_restroom']=''
+df['sl_wheelchair_ramps']=''
+df['sl_accepts_hiv_aids']=''
+df['sl_accepts_co_occurring_disorders']=''
+df['sl_accepts_chronic_illness']=''
+df['sl_accepts_physical_disabilities']='Y'
+df['sl_accepts_serious_mental_illness']=''
+df['sl_accepts_homeless_patients']=''
+df['sl_accepts_visually_impaired']=''
+df['sl_accepts_hearing_impaired']=''
+df['sl_adjustable_exam_table']=''
+df['sl_handicap_parking']='Y'
+df['sl_website_url']=''
+df['sl_esi_id_1']=''
+df['sl_esi_id_type_1']=''
+df['sl_esi_id_desc_1']=''
+df['sl_esi_id_eff_date_1']=''
+df['sl_esi_id_term_date_1']=''
+df['sl_esi_id_2']=''
+df['sl_esi_id_type_2']=''
+df['sl_esi_id_desc_2']=''
+df['sl_esi_id_eff_date_2']=''
+df['sl_esi_id_term_date_2']=''
+df['sl_esi_id_3']=''
+df['sl_esi_id_type_3']=''
+df['sl_esi_id_desc_3']=''
+df['sl_esi_id_eff_date_3']=''
+df['sl_esi_id_term_date_3']=''
+df['sl_esi_id_4']=''
+df['sl_esi_id_type_4']=''
+df['sl_esi_id_desc_4']=''
+df['sl_esi_id_eff_date_4']=''
+df['sl_esi_id_term_date_4']=''
+df['sl_esi_id_5']=''
+df['sl_esi_id_type_5']=''
+df['sl_esi_id_desc_5']=''
+df['sl_esi_id_eff_date_5']=''
+df['sl_esi_id_term_date_5']=''
+df['sl_service_location_name']=''
+df['sl_termination_date']=''
+df['spec_specialty_id_1']=df['Taxonomy Code']
+df['spec_reporting_type_1']=''
+df['spec_state_reporting_1']=''
+df['spec_specialty_id_2']=''
+df['spec_reporting_type_2']=''
+df['spec_state_reporting_2']=''
+df['spec_specialty_id_3']=''
+df['spec_reporting_type_3']=''
+df['spec_state_reporting_3']=''
+df['spec_specialty_id_4']=''
+df['spec_reporting_type_4']=''
+df['spec_state_reporting_4']=''
+df['spec_specialty_id_5']=''
+df['spec_reporting_type_5']=''
+df['spec_state_reporting_5']=''
+df['cert_specialty_id_1']=''
+df['cert_board_cert_date_1']=''
+df['cert_board_name_1']=''
+df['cert_expiration_date_1']=''
+df['cert_cert_name_1']=''
+df['cert_specialty_id_2']=''
+df['cert_board_cert_date_2']=''
+df['cert_board_name_2']=''
+df['cert_expiration_date_2']=''
+df['cert_cert_name_2']=''
+df['cert_specialty_id_3']=''
+df['cert_board_cert_date_3']=''
+df['cert_board_name_3']=''
+df['cert_expiration_date_3']=''
+df['cert_cert_name_3']=''
+df['aff_effective_date']=df['Delegate Committee Approval Date']
+df['aff_termination_date']='12/31/9999'
+df['aff_id']=''
+df['aff_contract_id']=''
+df['aff_pricing_driver_1']=''
+df['aff_pricing_driver_2']=''
+df['aff_pricing_driver_3']=''
+df['aff_cap_rate_id']=''
+df['lic_medicaid_1_id']=''
+df['lic_medicaid_1_eff_date']=''
+df['lic_medicaid_1_term_date']=''
+df['lic_state_1_id']=df['Medical Lic #']
+df['lic_state_1_state']='MD'
+df['lic_state_1_eff_date']='1/1/2020'
+df['lic_state_1_term_date']=df['Expiration Date']
+df['lic_state_2_id']=''
+df['lic_state_2_state']=''
+df['lic_state_2_eff_date']=''
+df['lic_state_2_term_date']=''
+df['lic_state_3_id']=''
+df['lic_state_3_state']=''
+df['lic_state_3_eff_date']=''
+df['lic_state_3_term_date']=''
+df['lic_dea_1_id']=df['DEA #']
+df['lic_dea_1_state']='MD'
+df['lic_dea_1_eff_date']='1/1/2020'
+df['lic_dea_1_term_date']=df['DEA Exp']
+df['lic_dea_2_id']=''
+df['lic_dea_2_state']=''
+df['lic_dea_2_eff_date']=''
+df['lic_dea_2_term_date']=''
+df['lic_dea_3_id']=''
+df['lic_dea_3_state']=''
+df['lic_dea_3_eff_date']=''
+df['lic_dea_3_term_date']=''
+df['ph_hos_rel_1_id']=''
+df['ph_hos_rel_1_eff_date']=''
+df['ph_hos_rel_1_term_date']=''
+df['ph_hos_rel_2_id']=''
+df['ph_hos_rel_2_eff_date']=''
+df['ph_hos_rel_2_term_date']=''
+df['ph_hos_rel_3_id']=''
+df['ph_hos_rel_3_eff_date']=''
+df['ph_hos_rel_3_term_date']=''
+#
+#reorder columns
+df = df[['prov_npi_id','prov_last_name','prov_first_name','prov_middle_initial','prov_name_suffix','prov_prof_desg_1','prov_soc_sec_number',
+           'prov_gender','prov_language_1','prov_language_2','prov_ethnicity','prov_birth_date','prov_email_address','prov_medicare_id',
+           'prov_medicaid_id','prov_cultural_competency_training','prov_dual_demo_population_training','prov_received_date','prov_approved_date',
+           'tax_id_number','bu_npi_id','bu_name','bu_address_1','bu_address_2','bu_city','bu_state','bu_zip_code','bu_mail_address_1',
+           'bu_mail_address_2','bu_mail_city','bu_mail_state','bu_mail_zip_code','bu_payment_center','bu_business_type','sl_address_1',
+           'sl_address_2','sl_city','sl_state','sl_zip_code','sl_county','sl_mail_address_1','sl_mail_address_2','sl_mail_city','sl_mail_state',
+           'sl_mail_zip_code','sl_office_manager_name','sl_phone_number','sl_fax_number','sl_monday_hours','sl_tuesday_hours','sl_wednesday_hours',
+           'sl_thursday_hours','sl_friday_hours','sl_saturday_hours','sl_sunday_hours','sl_status_code','sl_accept_new_patient',
+           'sl_min_patient_age','sl_max_patient_age','sl_offered_services','sl_show_in_dir','sl_tty_service','sl_handicap_accessible',
+           'sl_accept_dev_disabilities','sl_panel_capacity','sl_termination_reason','sl_24_operation','sl_loc_category','sl_translation_services',
+           'sl_wheelchair_accessible_exam_room','sl_wheelchair_accessible_restroom','sl_wheelchair_ramps','sl_accepts_hiv_aids',
+           'sl_accepts_co_occurring_disorders','sl_accepts_chronic_illness','sl_accepts_physical_disabilities','sl_accepts_serious_mental_illness',
+           'sl_accepts_homeless_patients','sl_accepts_visually_impaired','sl_accepts_hearing_impaired','sl_adjustable_exam_table',
+           'sl_handicap_parking','sl_website_url','sl_esi_id_1','sl_esi_id_type_1','sl_esi_id_desc_1','sl_esi_id_eff_date_1',
+           'sl_esi_id_term_date_1','sl_esi_id_2','sl_esi_id_type_2','sl_esi_id_desc_2','sl_esi_id_eff_date_2','sl_esi_id_term_date_2',
+           'sl_esi_id_3','sl_esi_id_type_3','sl_esi_id_desc_3','sl_esi_id_eff_date_3','sl_esi_id_term_date_3','sl_esi_id_4','sl_esi_id_type_4',
+           'sl_esi_id_desc_4','sl_esi_id_eff_date_4','sl_esi_id_term_date_4','sl_esi_id_5','sl_esi_id_type_5','sl_esi_id_desc_5',
+           'sl_esi_id_eff_date_5','sl_esi_id_term_date_5','sl_service_location_name','sl_termination_date','spec_specialty_id_1',
+           'spec_reporting_type_1','spec_state_reporting_1','spec_specialty_id_2','spec_reporting_type_2','spec_state_reporting_2',
+           'spec_specialty_id_3','spec_reporting_type_3','spec_state_reporting_3','spec_specialty_id_4','spec_reporting_type_4',
+           'spec_state_reporting_4','spec_specialty_id_5','spec_reporting_type_5','spec_state_reporting_5','cert_specialty_id_1',
+           'cert_board_cert_date_1','cert_board_name_1','cert_expiration_date_1','cert_cert_name_1','cert_specialty_id_2','cert_board_cert_date_2',
+           'cert_board_name_2','cert_expiration_date_2','cert_cert_name_2','cert_specialty_id_3','cert_board_cert_date_3','cert_board_name_3',
+           'cert_expiration_date_3','cert_cert_name_3','aff_effective_date','aff_termination_date','aff_id','aff_contract_id','aff_pricing_driver_1',
+           'aff_pricing_driver_2','aff_pricing_driver_3','aff_cap_rate_id','lic_medicaid_1_id','lic_medicaid_1_eff_date','lic_medicaid_1_term_date',
+           'lic_state_1_id','lic_state_1_state','lic_state_1_eff_date','lic_state_1_term_date','lic_state_2_id','lic_state_2_state',
+           'lic_state_2_eff_date','lic_state_2_term_date','lic_state_3_id','lic_state_3_state','lic_state_3_eff_date','lic_state_3_term_date',
+           'lic_dea_1_id','lic_dea_1_state','lic_dea_1_eff_date','lic_dea_1_term_date','lic_dea_2_id','lic_dea_2_state','lic_dea_2_eff_date',
+           'lic_dea_2_term_date','lic_dea_3_id','lic_dea_3_state','lic_dea_3_eff_date','lic_dea_3_term_date','ph_hos_rel_1_id',
+           'ph_hos_rel_1_eff_date','ph_hos_rel_1_term_date','ph_hos_rel_2_id','ph_hos_rel_2_eff_date','ph_hos_rel_2_term_date',
+           'ph_hos_rel_3_id','ph_hos_rel_3_eff_date','ph_hos_rel_3_term_date']]
+df = df.fillna('')
+
+
+df.to_csv('Meritus_Adds.csv',index=False)
+
+print('Success!')
+
+#Connect to MPMD Extract 
+cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                          "Server=aldprddbmpmd02.chicago.local;"
+                          "Database=MPMDAPP;"
+                          "Trusted_Connection=yes;")
+cursor = cnxn.cursor()
+sql ='''
+	select DISTINCT 
+p.hippa_id as prov_npi_id
+, bu.business_name as aldera_bu_name
+, bti.tax_id_number as tax_id_number
+, bu.npi_id as bu_npi_id
+, ll.address_1 as bu_address_1
+, l.address_1 as sl_address_1
+, cast(aff.termination_date as date) as aff_termination_date_y
+
+from MPMDAPP.dbo.provider_link sl with (nolock)
+
+left outer join MPMDAPP.dbo.provider p with (nolock) on (sl.provider_gid = p.provider_gid) and (p.record_status = 'a')
+
+left outer join MPMDAPP.dbo.business_units bu with (nolock) on (sl.business_gid = bu.business_gid) and (bu.record_status = 'a')
+
+left outer join MPMDAPP.dbo.Business_Tax_Relation btr with (nolock) on (bu.business_gid = btr.business_gid) and (btr.record_status = 'a')
+
+left outer join MPMDAPP.dbo.Business_Tax_Info bti with (nolock) on (btr.business_tax_gid = bti.business_tax_gid) and (bti.record_status = 'a')
+
+left outer join MPMDAPP.dbo.locations l with (nolock) on (sl.location_gid = l.location_gid) and (l.record_status = 'a')
+
+left outer join MPMDAPP.dbo.locations ll with (nolock) on (bu.payment_location_gid = ll.location_gid) and (ll.record_status = 'a') --and (bu.record_status = 'a')
+
+left outer join MPMDAPP.dbo.Provider_Affiliations aff with (nolock) on (sl.provider_gid = aff.provider_gid) and (sl.business_gid = aff.business_gid) and (sl.location_gid = aff.location_gid) and (aff.record_status = 'a')
+
+'''
+
+aldera = pd.read_sql(sql,cnxn)
+cursor.close()
+cnxn.close()
+
+#Copy header mapped file to df_mapped
+df_mapped = df.copy()
+
+#Check if Triad + SLs + BU on header mapped file (Roster) are already available in Aldera
+df_mapped["concat"] = df_mapped['prov_npi_id'].astype(str)+"_"+df_mapped['tax_id_number'].astype(str)+"_"+df_mapped['bu_npi_id'].astype(str)+ "_"+ df_mapped['sl_address_1'].str[:4].str.lower() + "_" + df_mapped['bu_address_1'].str[-5:]
+
+aldera['concat']= aldera['prov_npi_id'].astype(str)+"_"+aldera['tax_id_number'].astype(str)+"_"+aldera['bu_npi_id'].astype(str)+ "_"+ aldera['sl_address_1'].str[:4].str.lower() + "_" + aldera['bu_address_1'].str[-5:]
+aldera_con = aldera[['concat','aff_termination_date_y']]
+
+df_mapped_aldera = pd.merge(df_mapped,aldera_con,on=['concat'],how='left')
+df_mapped_aldera['Flag_1_if_available'] = np.where(df_mapped_aldera['aff_termination_date_y'].isnull(), '0','1')
+df_mapped_aldera.to_csv('Mapped_Meritus.csv',index=False)
+print('\nHeader file triad check is complete and file saved to drive')
+#input('Press Enter to continue')
+
+# Connect to Language codes from SSMS
+
+cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                          "Server=hpsprddb01.chicago.local\gemini;"
+                          "Database=ProviderSandbox;"
+                         "Trusted_Connection=yes;")
+cursor = cnxn.cursor()
+sql_language = '''SELECT  [Code]
+      ,[Description]
+      ,[New_Code]
+  FROM [ProviderSandbox].[Reference].[MPMD.Language]
+'''
+
+aldera_language = pd.read_sql(sql_language,cnxn)
+aldera_language.drop(columns =["Code"] , inplace =True)
+
+aldera_language.rename(columns={"Description": "prov_language_1"},inplace = True)
+
+df_mapped_aldera['prov_language_1']= df_mapped_aldera['prov_language_1'].str.lower()
+aldera_language['prov_language_1']= aldera_language['prov_language_1'].str.lower()
+
+#perform join on languages
+df_mapped_language = pd.merge(df_mapped_aldera,aldera_language, on='prov_language_1', how='left')
+df_mapped_language.rename(columns={"New_Code": "Languages_code"},inplace = True)
+
+df_mapped_language['var3'] = ''
+df_mapped_language['var3'] = np.where(df_mapped_language['Languages_code'].isnull(), df_mapped_language['prov_language_1'],df_mapped_language['Languages_code'])
+df_mapped_language['prov_language_1'] = df_mapped_language['var3']
+df_mapped_language.drop(['var3','Languages_code'],axis=1,inplace = True)
+df_l = df_mapped_language.copy()
+#df_language.to_csv('Mapped_HC3.csv',index=False)
+
+print('Language encoding is done!')
+
+#BU Name standardization from Aldera Extract
+df_l = df_l.astype(str).replace('nan','')
+
+
+#Replacing BU NAME from df with Aldera with NPI2 and TIN combo
+select_bu_columns = aldera[['tax_id_number','bu_npi_id','aldera_bu_name']]
+aldera_bu = select_bu_columns.copy()
+aldera_bu.drop_duplicates(inplace = True)
+
+print('\nJoining aldera data with dataframe')
+alderaJoin = pd.merge(df_l,aldera_bu, on=['tax_id_number','bu_npi_id'],  how='left')
+
+
+alderaJoin['bu_name'] = alderaJoin['aldera_bu_name']
+del alderaJoin['aldera_bu_name']
+
+alderaJoin.to_csv('Meritus_mapped.csv',index=False)
+#print('\nMapping of Bu name is complete and file saved to drive')
+
+
+import os
+print(os.getcwd())
